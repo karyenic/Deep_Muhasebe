114591eedbb1ae7cbdd597b18807fd08a8b05062
Adınız Soyadınız <eposta@adresiniz.com>
2025-08-10 16:29:02 +0300

    Veritabanı tabloları oluşturuldu ve SQLite dosyası eklendi.

---
+++ scripts/create_db.py
@@ -0,0 +1,7 @@
+﻿from core.database import create_tables
+from core.models.firma import Firma
+
+if __name__ == "__main__":
+    print("Veritabanı tabloları oluşturuluyor...")
+    create_tables()
+    print("Tablolar başarıyla oluşturuldu!")
